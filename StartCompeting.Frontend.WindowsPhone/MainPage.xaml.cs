dcade1c698f6ce8de3efb997de2498ae6ee2ae35
Jesper Johnsen <sojborg@outlook.com>
2014-04-29 23:48:23 +0200

    Added WindowsPhone app

---
+++ StartCompeting.Frontend.WindowsPhone/MainPage.xaml.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using StartCompeting.Frontend.WindowsPhone.Resources;
+using System.Device.Location;
+using System.Windows.Threading;
+using NExtra.Geo;
+using Microsoft.Phone.Maps.Controls;
+using System.Windows.Media;
+using Windows.Devices.Geolocation;
+using System.Windows.Shapes;
+
+namespace StartCompeting.Frontend.WindowsPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        private GeoCoordinateWatcher _watcher = new GeoCoordinateWatcher(GeoPositionAccuracy.High);
+        private MapPolyline _line;
+        private DispatcherTimer _timer = new DispatcherTimer();
+        private long _startTime;
+        private List<GeoCoordinate> _workoutCoordinates;
+
+
+        public MainPage()
+        {
+            InitializeComponent();
+            ShowMyLocationOnTheMap();
+
+            // create a line which illustrates the run
+            _line = new MapPolyline();
+            _line.StrokeColor = Colors.Red;
+            _line.StrokeThickness = 5;
+            Map.MapElements.Add(_line);
+
+            _watcher.PositionChanged += Watcher_PositionChanged;
+
+            _timer.Interval = TimeSpan.FromSeconds(1);
+            _timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object sender, EventArgs e)
+        {
+            TimeSpan runTime = TimeSpan.FromMilliseconds(System.Environment.TickCount - _startTime);
+            timeLabel.Text = runTime.ToString(@"hh\:mm\:ss");
+        }
+
+        private void StartButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (_timer.IsEnabled)
+            {
+                _watcher.Stop();
+                _timer.Stop();
+                SaveWorkout();
+                StartButton.Content = "Start";
+            }
+            else
+            {
+                _watcher.Start();
+                _timer.Start();
+                _startTime = System.Environment.TickCount;
+                StartButton.Content = "Stop";
+            }
+        }
+
+        private void SaveWorkout()
+        {
+
+        }
+
+        //ID_CAP_LOCATION
+        private double _kilometres;
+        private long _previousPositionChangeTick;
+
+        private void Watcher_PositionChanged(object sender, GeoPositionChangedEventArgs<GeoCoordinate> e)
+        {
+            var coord = new GeoCoordinate(e.Position.Location.Latitude, e.Position.Location.Longitude);
+
+            if (_line.Path.Count > 0)
+            {
+                _workoutCoordinates.Add(coord);
+                var previousPoint = _line.Path.Last();
+                var distance = coord.GetDistanceTo(previousPoint);
+                var millisPerKilometer = (1000.0 / distance) * (System.Environment.TickCount - _previousPositionChangeTick);
+                _kilometres += distance / 1000.0;
+
+                paceLabel.Text = TimeSpan.FromMilliseconds(millisPerKilometer).ToString(@"mm\:ss");
+                distanceLabel.Text = string.Format("{0:f2} km", _kilometres);
+                caloriesLabel.Text = string.Format("{0:f0}", _kilometres * 65);
+
+                PositionHandler handler = new PositionHandler();
+                var heading = handler.CalculateBearing(new Position(previousPoint), new Position(coord));
+                Map.SetView(coord, Map.ZoomLevel, heading, MapAnimationKind.Parabolic);
+
+                ShellTile.ActiveTiles.First().Update(new IconicTileData()
+                {
+                    Title = "StartCompeting",
+                    WideContent1 = string.Format("{0:f2} km", _kilometres),
+                    WideContent2 = string.Format("{0:f0} calories", _kilometres * 65),
+                });
+            }
+            else
+            {
+                Map.Center = coord;
+            }
+
+            _line.Path.Add(coord);
+            _previousPositionChangeTick = System.Environment.TickCount;
+        }
+
+        private async void ShowMyLocationOnTheMap()
+        {
+            // Get my current location.
+            Geolocator myGeolocator = new Geolocator();
+            Geoposition myGeoposition = await myGeolocator.GetGeopositionAsync();
+            Geocoordinate myGeocoordinate = myGeoposition.Coordinate;
+            GeoCoordinate myGeoCoordinate =
+            CoordinateConverter.ConvertGeocoordinate(myGeocoordinate);
+
+            // Make my current location the center of the Map.
+            this.Map.Center = myGeoCoordinate;
+            this.Map.ZoomLevel = 13;
+
+            // Create a small circle to mark the current location.
+            Ellipse myCircle = new Ellipse();
+            myCircle.Fill = new SolidColorBrush(Colors.Blue);
+            myCircle.Height = 20;
+            myCircle.Width = 20;
+            myCircle.Opacity = 50;
+
+            // Create a MapOverlay to contain the circle.
+            MapOverlay myLocationOverlay = new MapOverlay();
+            myLocationOverlay.Content = myCircle;
+            myLocationOverlay.PositionOrigin = new Point(0.5, 0.5);
+            myLocationOverlay.GeoCoordinate = myGeoCoordinate;
+
+            // Create a MapLayer to contain the MapOverlay.
+            MapLayer myLocationLayer = new MapLayer();
+            myLocationLayer.Add(myLocationOverlay);
+
+            // Add the MapLayer to the Map.
+            this.Map.Layers.Add(myLocationLayer);
+        }
+    }
+
+    public static class CoordinateConverter
+    {
+        public static GeoCoordinate ConvertGeocoordinate(Geocoordinate geocoordinate)
+        {
+            return new GeoCoordinate
+                (
+                geocoordinate.Latitude,
+                geocoordinate.Longitude,
+                geocoordinate.Altitude ?? Double.NaN,
+                geocoordinate.Accuracy,
+                geocoordinate.AltitudeAccuracy ?? Double.NaN,
+                geocoordinate.Speed ?? Double.NaN,
+                geocoordinate.Heading ?? Double.NaN
+                );
+        }
+    }
+}
