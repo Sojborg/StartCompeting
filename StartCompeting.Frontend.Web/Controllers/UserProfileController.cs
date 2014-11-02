cc82c822bd7e6a1184fbaaf60638c3265fefe71e
Jesper <jsj@d60.dk>
2014-11-02 22:08:13 +0100

    Added user profiles pages

---
+++ StartCompeting.Frontend.Web/Controllers/UserProfileController.cs
@@ -0,0 +1,17 @@
+﻿using System.Web.Mvc;
+
+namespace StartCompeting.Frontend.Web.Controllers
+{
+    public class UserProfileController : Controller
+    {
+        public UserProfileController()
+        {
+            
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ StartCompeting.Frontend.Web/Scripts/controllers/userProfileController.js
@@ -0,0 +1,27 @@
+﻿app.controller('UserProfileController', function($scope, $http) {
+
+    var serviceUrl = '/StartCompeting/api/';
+    loadUserProfileById(1);
+
+    $scope.formData = {
+
+    };
+
+    $scope.submit = function () {
+        if (FormData.id == 0)
+            createRace($scope.formData);
+        else
+            updateRace($scope.formData);
+    };
+
+    function loadUserProfileById(id) {
+        $http.get(serviceUrl + "UserProfile?id=" + id)
+            .success(function (data) {
+                $scope.races = data.Races;
+                $scope.workouts = data.Workouts;
+                //$scope.formData.name =     data.Name;
+                //$scope.formData.raceTypeId = data.RaceTypeId;
+                //$scope.formData.raceLength = data.RaceLength;
+        });
+    }
+});
