9119d75300766c5232ed04fd3140c6269ab51d9c
Jesper <jsj@d60.dk>
2014-11-02 22:08:35 +0100

    Added bower components

---
+++ Infrastruture/Services/UserProfileService.cs
@@ -0,0 +1,13 @@
+﻿using Core.Interfaces;
+using Core.Models;
+
+namespace Infrastruture.Services
+{
+    public class UserProfileService : IUserProfileService
+    {
+        public User GetUserProfile()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
