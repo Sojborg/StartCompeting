5adbf1f1ac8f513fddce1b316566198609351785
Jesper <jsj@d60.dk>
2015-02-22 23:22:17 +0100

    Start moving away from bootstrap

---
+++ StartCompeting.Frontend.Web/Client/app/workout/new-workout-controller.js
@@ -0,0 +1,51 @@
+﻿app.controller('NewWorkoutController', function ($scope, workoutService, $routeParams, $location) {
+
+    $scope.formData = {
+    };
+
+    $scope.getWorkout = function (id) {
+        workoutService.loadWorkoutById(id).then(function (data) {
+            $scope.formData = data;
+        });
+    };
+
+    $scope.submit = function () {
+        if ($scope.formData.id == undefined || $scope.formData.id == 0)
+            workoutService.createWorkout($scope.formData).then(function (data) {
+                if (data) {
+                    $scope.resetWorkout();
+                }
+            });
+        else {
+
+            workoutService.updateWorkout($scope.formData).then(function (data) {
+                if (data) {
+                    $scope.resetWorkout();
+                }
+            });
+        }
+    };
+
+    $scope.resetWorkout = function () {
+        $location.path('/workouts');
+    }
+
+    $scope.loadRaceTypes = function () {
+        workoutService.loadRaceTypes().then(function (data) {
+            $scope.raceTypes = data;
+        },
+        function (errorMessage) {
+            $scope.error = errorMessage;
+        });
+
+    }
+
+    var workoutId = $routeParams.workoutId;
+    $scope.loadRaceTypes();
+    if (workoutId !== undefined && workoutId != 0)
+        $scope.getWorkout(workoutId);
+
+    $scope.cancel = function () {
+        $location.path('/workouts');
+    };
+});
