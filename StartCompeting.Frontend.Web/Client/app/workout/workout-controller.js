3d87e416d2dbfc6696e3ee0a80082ca3167bfc04
Jesper <jsj@d60.dk>
2015-02-26 22:45:17 +0100

    Added datatables js component

--- StartCompeting.Frontend.Web/Client/app/workout/workout-controller.js
+++ StartCompeting.Frontend.Web/Client/app/workout/workout-controller.js
@@ -3,26 +3,6 @@
     $scope.orderByField = 'start';
     $scope.reverseSort = false;
 
-    $scope.open = function (workoutId) {
-        
-        //var modalInstance = $modal.open({
-        //    templateUrl: 'Client/app/workout/workout-modal.html',
-        //    controller: 'ModalInstanceCtrl',
-        //    size: '',
-        //    resolve: {
-        //        workoutId: function () {
-        //            return workoutId;
-        //        }
-        //    }
-        //});
-
-        //modalInstance.result.then(function () {
-        //    $scope.loadWorkouts();
-        //}, function () {
-        //    //$log.info('Modal dismissed at: ' + new Date());
-        //});
-    };
-
     $scope.loadWorkouts = function () {
         workoutService.loadWorkouts().then(function (data) {
             $scope.workouts = data;
