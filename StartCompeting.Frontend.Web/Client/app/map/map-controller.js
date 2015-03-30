81f1e166a359170c3a3cd36a0e0f374b203d5888
Jesper <jsj@d60.dk>
2015-03-30 22:15:51 +0200

    added client side map directive

---
+++ StartCompeting.Frontend.Web/Client/app/map/map-controller.js
@@ -0,0 +1,34 @@
+﻿
+app.controller("MapController", mapController);
+
+function mapController($scope) {
+    $scope.callback = function (map) {
+        map.setView([51.433333, 5.483333], 12);
+    };
+}
+
+
+app.directive('mapbox', [
+    function () {
+        return {
+            restrict: 'EA',
+            replace: true,
+            scope: {
+                callback: "="
+            },
+            template: '<div></div>',
+            link: function (scope, element, attributes) {
+                L.mapbox.accessToken = 'pk.eyJ1Ijoic29qYm9yZyIsImEiOiJ4engxV1pZIn0.rbLiVa797JFI-ouJqy5Ytw';
+                var map = L.mapbox.map('map', 'sojborg.l1nm7ahj');
+
+                var polyline = L.polyline([]).addTo(map);
+
+                polyline.addLatLng(
+                    L.latLng(
+                        ))
+
+                scope.callback(map);
+            }
+        };
+    }
+]);
